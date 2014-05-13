65c599d6ca70dd74e055552151147ff873e241a1
Isaque Dutra <dutra@mit.edu>
2014-05-13 15:11:13 -0400

    Intialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlSky::Application.config.secret_token = '26e53d9e960d6e1f84fd718a5745d126d66f3f45267ac26adccb2504bad450e0b5d07185fab3205efd6e66991eb015bb65e5d8fdebdd7699a3086a7c3fd0dc72'
